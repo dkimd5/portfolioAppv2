1b7fa33fc20bd86f24505e024ac5b2b347e875e1
dkimd5 <dkimd5@mail.ru>
2022-01-10 18:02:40 +0300

    react router dom added, home element added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <div className="noise"></div>
       <div className="App">
-        
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
       </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Dmitrii Kim
+        </div>
+    )
+}
+
+export default Home
