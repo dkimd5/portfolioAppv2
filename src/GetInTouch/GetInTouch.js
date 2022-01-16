ee2346d1dfebf76bfbf113bf1e5d61ebe68e23e0
dkimd5 <dkimd5@mail.ru>
2022-01-16 14:22:02 +0300

    GetInTouch files created

---
+++ src/GetInTouch/GetInTouch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GetInTouch.css';
+
+function GetInTouch() {
+    return (
+        <div>
+            get
+        </div>
+    )
+}
+
+export default GetInTouch
